3ea6e58cd703305cfafb6e0188a7c02197c18c6d
Monceber <monceber.am@gmail.com>
2021-05-27 14:11:00 +0300

    Remove ignoring cross-staff for artics in beams
    - removed code that prevented cross-staff placement for articulations in cross-staff beams

--- src/artic.cpp
+++ src/artic.cpp
@@ -295,8 +295,6 @@ int Artic::CalcArtic(FunctorParams *functorParams)
     Layer *layer = vrv_cast<Layer *>(this->GetFirstAncestor(LAYER));
     assert(layer);
 
-    Beam *beam = dynamic_cast<Beam *>(this->GetFirstAncestor(BEAM));
-
     if (params->m_parent->m_crossLayer) {
         layer = params->m_parent->m_crossLayer;
     }
@@ -339,21 +337,10 @@ int Artic::CalcArtic(FunctorParams *functorParams)
     if (this->GetDrawingPlace() == STAFFREL_above && params->m_crossStaffAbove) {
         this->m_crossStaff = params->m_staffAbove;
         this->m_crossLayer = params->m_layerAbove;
-        // Exception - the artic is above in a cross-staff note / chord going down - the positioning is relative to the
-        // parent where the beam is
-        if (beam && beam->m_crossStaffContent && !beam->m_crossStaff && beam->m_crossStaffRel == STAFFREL_basic_below) {
-            this->m_crossStaff = NULL;
-            this->m_crossLayer = NULL;
-        }
     }
     else if (this->GetDrawingPlace() == STAFFREL_below && params->m_crossStaffBelow) {
         this->m_crossStaff = params->m_staffBelow;
         this->m_crossLayer = params->m_layerBelow;
-        // Exception - opposite as above
-        if (beam && beam->m_crossStaffContent && !beam->m_crossStaff && beam->m_crossStaffRel == STAFFREL_basic_above) {
-            this->m_crossStaff = NULL;
-            this->m_crossLayer = NULL;
-        }
     }
 
     return FUNCTOR_CONTINUE;
