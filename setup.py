87024cbd692bf3c2ed304a36e5b59350688dcfeb
Klaus Rettinghaus <klaus.rettinghaus@gmail.com>
2024-01-20 17:36:53 +0100

    set minimum to 3.8

--- setup.py
+++ setup.py
@@ -156,7 +156,7 @@ setup(name='verovio',
           'verovio.data.Petaluma': os.listdir('./data/Petaluma'),
           'verovio.data.text': os.listdir('./data/text'),
       },
-      python_requires='>=3.7',
+      python_requires='>=3.8',
       project_urls={
           'Bug Reports': 'https://github.com/rism-digital/verovio/issues',
           'Source': 'https://github.com/rism-digital/verovio',
